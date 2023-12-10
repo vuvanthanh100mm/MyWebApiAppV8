69e427716e72652c85ae328fd7b283e73ae7defb
vuvanthanh100mm <104214255+vuvanthanh100mm@users.noreply.github.com>
2023-12-10 18:14:04 +0700

    video 9
    https://www.youtube.com/watch?v=RfGNCLnw3tg

---
+++ MyWebApiAppV8/Controllers/ShirtsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApiAppV8.Models;
+
+namespace MyWebApiAppV8.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById2(int id)
+        {
+            return $"Reading all the shirts: {id}";
+        }
+
+        /*[HttpGet("{id}")]
+        public string GetShirtById2(int id, [FromQuery] string color)
+        {
+            return $"Reading all the shirts: {id}, {color}";
+        }
+
+        [HttpGet("{id}")]
+        public string GetShirtById(int id, [FromHeader(Name ="Color")] string color)
+        {
+            return $"Reading all the shirts: {id}, {color}";
+        }*/
+
+        [HttpPost]
+        public string CreateShirts([FromBody] Shirt shirt)
+        {
+            return $"Creating a  shirts";
+        }
+
+        [HttpPut("{id}")]
+        public string UpdateShirtById(int id)
+        {
+            return $"Updating all the shirts: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirtById(int id)
+        {
+            return $"Deleting all the shirts: {id}";
+        }
+    }
+}
---
+++ MyWebApiAppV8/Models/Shirt.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApiAppV8.Models
+{
+    public class Shirt
+    {
+        public int ShirtId { get; set; }
+
+        [Required]
+        public string? Brand { get; set; }
+
+        [Required]
+        public string? Color { get; set; }
+        public int Size { get; set; }
+
+        [Required]
+        public string? Gender { get; set; }
+
+        public double Price { get; set; }
+
+    }
+}
